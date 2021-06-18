aa074cd43ea57fb6b4d9bf041b6b4e4b8d0e1822
ComputadorasySensores <79330985+ComputadorasySensores@users.noreply.github.com>
2021-06-18 18:38:35 -0300

    Create 0a180.py
    Posicionamiento de un servo en 0 y 180 grados. Corresponde a capítulo 18 de Computadoras y Sensores en Youtube.

---
+++ 0a180.py
@@ -0,0 +1,12 @@
+from machine import Pin, PWM
+import time
+
+servo = PWM(Pin(0))
+servo.freq(50)
+
+while True:
+    servo.duty_u16(1311)
+    time.sleep(2)
+    servo.duty_u16(7864)
+    time.sleep(2)
+    
